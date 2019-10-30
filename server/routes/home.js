ae15a9ccfc19cd39bd6bc8f12d43fb4c09b73127
Konstantin Gorshkov <const.potter@gmail.com>
2019-10-30 17:54:28 +0600

    Added support pug
    - Home page rewrite on pug
    - Added login page

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,10 +1,13 @@
-const fs = require('fs');
+const fs = require('fs'),
+      pug = require('pug');
 
-module.exports = (req, res) => {
-    fs.readFile('./client/index.html', (err, file) => {
-        if(err) {
-            console.log(err)
-        }
-        res.end(file)
-    })
+module.exports = (client) => {
+    const loggedUser = client.session.get('login');
+    let html;
+    if(loggedUser) {
+        html = pug.renderFile('server/views/chat-page.pug', {login: loggedUser});
+    } else {
+        html = pug.renderFile('server/views/login.pug');
+    }
+    client.send(html);
 }
